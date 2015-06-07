9e6e8630ec01fd9176cbad01ed343d878593288c
Daniel Berrocal <jdanielb1992@gmail.com>
2015-06-06 19:26:23 -0600

    multi character image

--- Neurona 2/Neurona/Index.aspx.cs
+++ Neurona 2/Neurona/Index.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
@@ -13,22 +14,7 @@ namespace Neurona
 {
     public partial class Index : System.Web.UI.Page
     {
-
-
-        int[,] matriz = new int[10, 10] { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
-                                          { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
-                                          { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
-                                          { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
-                                          { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
-                                          { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
-                                          { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
-                                          { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
-                                          { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                                          { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
-
-        int x = 0;
-        int y = 0;
-        
+                
         protected void Page_Load(object sender, EventArgs e)
         {
             
@@ -51,44 +37,13 @@ namespace Neurona
 
         protected void evaluate(object sender, EventArgs e)
         {
-            Neurotron insNeurotron = new Neurotron();
+            //Neurotron insNeurotron = new Neurotron();
+            //result.Text = insNeurotron.checkMatrix(getMatrixFromIMG(),Convert.ToDouble(learning.Text));
 
-            if (check.Checked)
-            {
-                result.Text = insNeurotron.checkMatrix(getMatrixFromIMG(),Convert.ToDouble(learning.Text));
-            }
-            else
-            {
-                result.Text = insNeurotron.checkMatrix(getMatriz(), Convert.ToDouble(learning.Text)); 
-            }
-
+            ArrayList x = getMatrixFromIMG();
         }
 
-        protected void paintButton(object sender, EventArgs e)
-        {
-            System.Web.UI.WebControls.Button btn = ((System.Web.UI.WebControls.Button)sender);
-            string id = btn.ID;
-            string posx = id.Substring(1,1);
-            string posy = id.Substring(2);
-            x = Convert.ToInt32(posx);
-            y = Convert.ToInt32(posy);
-
-            
-
-            Color _color = btn.BackColor;
-            if (_color.Equals(System.Drawing.ColorTranslator.FromHtml("#1186ab")))
-            {
-                btn.BackColor = Color.Black;
-
-                
-            }
-            else
-            {
-                btn.BackColor = System.Drawing.ColorTranslator.FromHtml("#1186ab");
-                
-            }
-           
-        }
+        
 
         static public Bitmap ScaleImage(System.Drawing.Image image, int maxWidth, int maxHeight)
         {
@@ -106,60 +61,82 @@ namespace Neurona
             return bmp;
         }
 
-        public int[] getMatriz()
+        protected Bitmap getBitmapImage()
         {
-            int[] vector = new int[100];
-            int cont = 0;
-            for (int i = 0; i < 10; i++)
-            {
-                for (int j = 0; j < 10; j++)
-                {
-                    System.Web.UI.WebControls.Button btn = ((System.Web.UI.WebControls.Button)FindControl("b"+i.ToString()+j.ToString()));
-                    if (btn.BackColor.Equals(System.Drawing.ColorTranslator.FromHtml("#1186ab")))
-                    {
-                        vector[cont] = 1;
-                        matriz[i, j] = 1;
-                    }
-                    else
-                    {
-                        vector[cont] = 0;
-                        matriz[i, j] = 0;
-                    }
-                    cont++;
-                }
-            }
-            return vector;
-        }
-
-        protected int[] getMatrixFromIMG() {
             image.PostedFile.SaveAs(Server.MapPath("~/Upload/") + Path.GetFileName(image.PostedFile.FileName));
             string file = Server.MapPath("Upload/" + image.PostedFile.FileName);
             img.ImageUrl = "~/Upload/" + image.PostedFile.FileName;
             Bitmap myBitmap = GrayScale(new Bitmap(file));
-            int[] vector = new int[100];
-            int cont = 0;
+            return myBitmap;
+        }
+
+        protected bool isCleanColumn(Bitmap img, int colum)
+        {
+            bool clean = true;
             for (int i = 0; i < 10; i++)
             {
-                for (int j = 0; j < 10; j++)
+                Color pixelColor = img.GetPixel(colum, i);
+                Color color = System.Drawing.ColorTranslator.FromHtml("#FFFFFF");
+                if (pixelColor.Equals(color))
                 {
-                    Color pixelColor = myBitmap.GetPixel(j, i);
-                    Color color = System.Drawing.ColorTranslator.FromHtml("#FFFFFF");
-                    if (pixelColor.Equals(color))
-                    {
-                        vector[cont] = 0;
-                        matriz[j, i] = 0;
-                    }
-                    else
-                    {
-                        vector[cont] = 1;
-                        matriz[j, i] = 1;
-                    }
-                    cont++;
-
+                    clean = true;
                 }
-
+                else
+                {
+                    clean = false;
+                    break;
+                }
             }
-            return vector;
+                return clean;
+        }
+
+        protected ArrayList getMatrixFromIMG() {
+            Bitmap myBitmap = getBitmapImage();
+            int[] vector = new int[100];
+            int cont = 0;
+            Color color = System.Drawing.ColorTranslator.FromHtml("#FFFFFF");
+            ArrayList matriz = new ArrayList();
+            
+
+            for (int c = 0; c < myBitmap.Width; c += 10)
+            {
+                bool cleanColumn = isCleanColumn(myBitmap, c);
+                if (!cleanColumn)
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        for (int j = c; j < c+10; j++)
+                        {
+
+                            Color pixelColor = myBitmap.GetPixel(j, i);
+
+                            if (pixelColor.Equals(color))
+                            {
+                                vector[cont] = 0;
+                                
+                            }
+                            else
+                            {
+                                vector[cont] = 1;
+                                
+                            }
+                            cont++;
+                        }
+                    }
+
+                matriz.Add(vector);
+                }
+                else
+                {
+                    c-=9;
+                }
+                cont = 0;
+                
+                vector = new int[100];
+                
+            }
+            
+            return matriz;
         }
     }
 }
